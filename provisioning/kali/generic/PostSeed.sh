4d5591518cc0f17103242faacd73e5bde6c2f524
J0nan <jagilcarrera@proton.me>
2024-10-07 01:17:05 +0200

    removed sudo

--- provisioning/kali/generic/PostSeed.sh
+++ provisioning/kali/generic/PostSeed.sh
@@ -3,34 +3,34 @@
 # Installs and configures multiple hacking tools
 
 # Install requiered packages
-sudo apt install -y python3.12-venv
-sudo apt install -y golang-go
-sudo apt install -y docker.io
-sudo apt install -y google-android-platform-tools-installer
+apt install -y python3.12-venv
+apt install -y golang-go
+apt install -y docker.io
+apt install -y google-android-platform-tools-installer
 
 # Testssl.sh - https://github.com/drwetter/testssl.sh
-sudo git clone https://github.com/drwetter/testssl.sh.git /opt/testssl.sh
-sudo ln -s /opt/testssl.sh/testssl.sh /usr/bin/testssl
+git clone https://github.com/drwetter/testssl.sh.git /opt/testssl.sh
+ln -s /opt/testssl.sh/testssl.sh /usr/bin/testssl
 
 # Aquatone - https://github.com/firefart/aquatone
-sudo git clone https://github.com/firefart/aquatone.git /opt/aquatone
+git clone https://github.com/firefart/aquatone.git /opt/aquatone
 cd /opt/aquatone
-sudo go build /opt/aquatone
-sudo ln -s /opt/aquatone/aquatone /usr/bin/aquatone
+go build /opt/aquatone
+ln -s /opt/aquatone/aquatone /usr/bin/aquatone
 
 # ssh-audit - https://github.com/jtesta/ssh-audit
-sudo git clone https://github.com/jtesta/ssh-audit.git /opt/ssh-audit
-sudo ln -s /opt/ssh-audit/ssh-audit.py /usr/bin/ssh-audit
+git clone https://github.com/jtesta/ssh-audit.git /opt/ssh-audit
+ln -s /opt/ssh-audit/ssh-audit.py /usr/bin/ssh-audit
 
 # Terrapin-Scanner - https://github.com/RUB-NDS/Terrapin-Scanner
-sudo git clone https://github.com/RUB-NDS/Terrapin-Scanner.git /opt/Terrapin-Scanner
+git clone https://github.com/RUB-NDS/Terrapin-Scanner.git /opt/Terrapin-Scanner
 cd /opt/Terrapin-Scanner
-sudo go build /opt/Terrapin-Scanner
-sudo ln -s /opt/Terrapin-Scanner/Terrapin-Scanner /usr/bin/Terrapin-Scanner
+go build /opt/Terrapin-Scanner
+ln -s /opt/Terrapin-Scanner/Terrapin-Scanner /usr/bin/Terrapin-Scanner
 
 # JexBoss - https://github.com/joaomatosf/jexboss
-sudo git clone https://github.com/joaomatosf/jexboss.git /opt/jexboss
+git clone https://github.com/joaomatosf/jexboss.git /opt/jexboss
 cd /opt/jexboss
-sudo python3 -m venv /opt/jexboss
-sudo /opt/jexboss/bin/python /opt/jexboss/bin/pip install -r requires.txt
-sudo ln -s /opt/jexboss/jexboss.py /usr/bin/jexboss
+python3 -m venv /opt/jexboss
+/opt/jexboss/bin/python /opt/jexboss/bin/pip install -r requires.txt
+ln -s /opt/jexboss/jexboss.py /usr/bin/jexboss
