a140fbc5a54713bbd790e05b2d254bc781c57cbd
J0nan <46595866+J0nan@users.noreply.github.com>
2024-12-29 01:46:00 +0100

    Update PostSeed.sh

--- provisioning/kali/generic/PostSeed.sh
+++ provisioning/kali/generic/PostSeed.sh
@@ -20,8 +20,11 @@ mkdir -p /home/kali/.config/qterminal.org
 wget --no-check-certificate -O /home/kali/.config/qterminal.org/qterminal.ini https://raw.githubusercontent.com/J0nan/RandomThings/refs/heads/develop/provisioning/kali/generic/qterminal.ini
 wget --no-check-certificate -O /home/kali/.config/qterminal.org/qterminal_bookmarks.xml https://raw.githubusercontent.com/J0nan/RandomThings/refs/heads/develop/provisioning/kali/generic/qterminal_bookmarks.xml
 chown -R kali:kali /home/kali/.config/qterminal.org
+# Copy the old terminal themes to the new qterminal
 mkdir -p /usr/share/qtermwidget6/color-schemes
-cp /usr/share/qtermwidget5/color-schemes/* /usr/share/qtermwidget6/color-schemes/  # Copy the old terminal themes to the new
+cp /usr/share/qtermwidget5/color-schemes/* /usr/share/qtermwidget6/color-schemes/
+# Setting the default kali wallpaper
+mkdir -p /home/kali/.config/xfce4/xfconf/xfce-perchannel-xml 
 wget --no-check-certificate -O /home/kali/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml https://raw.githubusercontent.com/J0nan/RandomThings/refs/heads/develop/provisioning/kali/generic/xfce4-desktop.xml
 chown -R kali:kali /home/kali/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
 
