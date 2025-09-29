a80e7ae1e1a75a64920d60a03fed0ffe5103bb83
J0nan <46595866+J0nan@users.noreply.github.com>
2025-09-29 08:03:46 +0200

    Added kali to Vbox group

--- provisioning/kali/generic/PostSeed.sh
+++ provisioning/kali/generic/PostSeed.sh
@@ -32,6 +32,9 @@ chown -R kali:kali /home/kali/.config/qterminal.org
 chsh -s /bin/zsh root
 chsh -s /bin/zsh kali
 
+# Add kali user to vboxsf group, needed to access shared folders
+sudo usermod -aG vboxsf kali
+
 # Power manager XFCE in user kali
 # Disabling power safe, blank screen and switch-off in the monitor
 mkdir -p /home/kali/.config/xfce4/xfconf/xfce-perchannel-xml
